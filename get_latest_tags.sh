85a36f6233d314f8c18dc9e3b322286ac28a2dd1
Alistair Hughes <ali.fhughes@gmail.com>
2023-05-18 09:39:36 +0100

    save changes

--- get_latest_tags.sh
+++ get_latest_tags.sh
@@ -9,25 +9,48 @@ for directory in $directories; do
   if [ -d "$directory/.git" ]; then
     # Get the repo name from the directory name
     repo_name=$(basename "$directory")
-
+    
     # Change to the repository directory
     cd "$directory"
-
+    
     # Perform a git fetch to get the latest information (hide output)
     git fetch --tags >/dev/null 2>&1
-
+    
+    # Save the current branch name
+    current_branch=$(git symbolic-ref --short HEAD)
+    
+    # Check if there are any changes in the current branch
+    if ! git diff-index --quiet HEAD --; then
+      # Stash the changes
+      git stash save --include-untracked >/dev/null 2>&1
+    fi
+    
+    # Switch to the master branch
+    git checkout master >/dev/null 2>&1
+    
     # Get the latest git tag
     latest_tag=$(git describe --tags --abbrev=0 2>/dev/null)
-
+    
     # If there are no tags, skip to the next directory
     if [ -z "$latest_tag" ]; then
+      # Switch back to the original branch
+      git checkout "$current_branch" >/dev/null 2>&1
       cd ..
       continue
     fi
-
+    
     # Output the repository name and latest tag
     echo "$repo_name - $latest_tag"
-
+    
+    # Switch back to the original branch
+    git checkout "$current_branch" >/dev/null 2>&1
+    
+    # Apply the stashed changes
+    if git stash apply --quiet; then
+      # Clear the applied stash
+      git stash drop >/dev/null 2>&1
+    fi
+    
     # Change back to the original directory
     cd ..
   fi
