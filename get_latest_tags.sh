d1339445fde4f2f6e1db343861bf8bf8138260f3
Alistair Hughes <ali.fhughes@gmail.com>
2023-05-18 09:25:20 +0100

    small bits

--- get_latest_tags.sh
+++ get_latest_tags.sh
@@ -9,24 +9,26 @@ for directory in $directories; do
   if [ -d "$directory/.git" ]; then
     # Get the repo name from the directory name
     repo_name=$(basename "$directory")
-    
+
     # Change to the repository directory
     cd "$directory"
-    
+
+    # Perform a git fetch to get the latest information
+    git fetch --tags
+
     # Get the latest git tag
     latest_tag=$(git describe --tags --abbrev=0 2>/dev/null)
-    
+
     # If there are no tags, skip to the next directory
     if [ -z "$latest_tag" ]; then
       cd ..
       continue
     fi
-    
+
     # Output the repository name and latest tag
     echo "$repo_name - $latest_tag"
-    
+
     # Change back to the original directory
     cd ..
   fi
 done
-
